6e622e95c8873d80c78f0fd94ced6805cef9f27e
anassboussarhan <anass.boussarhan@gmail.com>
2022-08-12 14:26:54 +0200

    Update streamlit-app.py
    added header and text

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,3 +1,9 @@
 import streamlit
-streamlit.title("My parents")
-streamlit.text('Fixed typo')
+streamlit.title('My parents healthy dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text("🥑Avocado toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+             
